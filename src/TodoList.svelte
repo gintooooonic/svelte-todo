c57380547d9b844323bf8112e7d98dad3a8bac6e
Woojin Shin <gintooooonic@gmail.com>
2022-04-24 19:35:11 +0900

    Add list items on TodoList

---
+++ src/TodoList.svelte
@@ -0,0 +1,20 @@
+<ul>
+  {#each items as item}
+    <li>
+      <label>
+        <input type="checkbox" checked={item.done}/>
+        {item.content}
+      </label>
+    </li>
+  {/each}
+</ul>
+
+<script>
+  const items = [
+    { content: 'Fork', date: '2022-04-24', done: true },
+    { content: 'Clone', date: '2022-04-24', done: true },
+    { content: 'Commit', date: '2022-04-25', done: true },
+    { content: 'Push', date: '2022-04-25', done: false },
+    { content: 'PR', date: '2022-04-25', done: false },
+  ]
+</script>
