0ea74914da21a936b761aa4826a02898512528ee
Woojin Shin <gintooooonic@gmail.com>
2022-04-24 21:41:40 +0900

    Add buttons on DateNav

--- src/DateNav.svelte
+++ src/DateNav.svelte
@@ -1,5 +1,11 @@
 <div>
+  <span class="button">
+    <img src="assets/images/arrow-left.svg" alt="button"/>
+  </span>
   <h1>{dateStr}</h1>
+  <span class="button">
+    <img src="assets/images/arrow-right.svg" alt="button"/>
+  </span>
 </div>
 
 <script>
@@ -8,3 +14,12 @@
   const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
   const dateStr = `${DAYS[date.getDay()]}, ${MONTHS[date.getMonth()]} ${date.getDate()}`
 </script>
+
+<style>
+  .button {
+    display: inline-block;
+    width: 32px;
+    height: 32px;
+    cursor: pointer;
+  }
+</style>
