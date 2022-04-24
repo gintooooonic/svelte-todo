e5488187803227b80f5ab5db617c1fd0145f84ae
Woojin Shin <gintooooonic@gmail.com>
2022-04-24 19:56:12 +0900

    Display date on DateNav

---
+++ src/DateNav.svelte
@@ -0,0 +1,10 @@
+<div>
+  <h1>{dateStr}</h1>
+</div>
+
+<script>
+  const date = new Date()
+  const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat']
+  const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  const dateStr = `${DAYS[date.getDay()]}, ${MONTHS[date.getMonth()]} ${date.getDate()}`
+</script>
