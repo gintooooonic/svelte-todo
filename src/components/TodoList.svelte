86d30aa7d43c5ecee3af1e97dab26302b8450089
Woojin Shin <gintooooonic@gmail.com>
2022-04-27 17:43:38 +0900

    Get todo data from DB

--- src/components/TodoList.svelte
+++ src/components/TodoList.svelte
@@ -1,15 +1,20 @@
 <ul>
-  {#each items.filter(item => isDateEqual(item.date, date)) as item}
-    <li>
-      <label>
-        <input type="checkbox" checked={item.done}/>
-        {item.content}
-      </label>
-    </li>
-  {/each}
+  {#await todoPromise then items}
+    {#each items.filter(item => isDateEqual(item.date, date)) as item}
+      <li>
+        <label>
+          <input type="checkbox" checked={item.done}/>
+          {item.content}
+        </label>
+      </li>
+    {/each}
+  {/await}
 </ul>
 
 <script>
+import { getTodoAll } from "../database";
+
+
   export let date
 
   function isDateEqual(d1, d2) {
@@ -18,13 +23,7 @@
       && d1.getDate() === d2.getDate()
   }
 
-  const items = [
-    { content: 'Fork', date: new Date(2022, 3, 27), done: true },
-    { content: 'Clone', date: new Date(2022, 3, 27), done: true },
-    { content: 'Commit', date: new Date(2022, 3, 28), done: true },
-    { content: 'Push', date: new Date(2022, 3, 28), done: false },
-    { content: 'PR', date: new Date(2022, 3, 28), done: false },
-  ]
+  const todoPromise = getTodoAll()
 </script>
 
 <style>
