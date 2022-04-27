a3d4d8e7a29522cb3bb6586bebe3a840ae8c3a91
Woojin Shin <gintooooonic@gmail.com>
2022-04-27 18:29:47 +0900

    Add TodoForm component

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,19 @@
+<form on:submit|preventDefault = {handleSubmit}>
+  <input type="text" name="content" bind:value={content} placeholder="New Todo..."/>
+  <input type="submit"/>
+</form>
+
+<script>
+  import { putTodo } from "../database";
+
+  export let date
+  let content = ''
+
+  function handleSubmit() {
+    const todo = {
+      content, date,
+      done: false
+    }
+    putTodo(todo)
+  }
+</script>
--- src/components/TodoList.svelte
+++ src/components/TodoList.svelte
@@ -1,19 +1,22 @@
-<ul>
-  {#await todoPromise then items}
-    {#each items.filter(item => isDateEqual(item.date, date)) as item}
-      <li>
-        <label>
-          <input type="checkbox" checked={item.done}/>
-          {item.content}
-        </label>
-      </li>
-    {/each}
-  {/await}
-</ul>
+<div>
+  <ul>
+    {#await todoPromise then items}
+      {#each items.filter(item => isDateEqual(item.date, date)) as item}
+        <li>
+          <label>
+            <input type="checkbox" checked={item.done}/>
+            {item.content}
+          </label>
+        </li>
+      {/each}
+    {/await}
+  </ul>
+  <TodoForm {date}/>
+</div>
 
 <script>
-import { getTodoAll } from "../database";
-
+  import { getTodoAll } from "../database";
+  import TodoForm from "./TodoForm.svelte";
 
   export let date
 
